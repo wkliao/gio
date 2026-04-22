e31380ec9ef9c17685dd3c0d92e9592e3662b452
Wei-keng Liao <wkliao@users.noreply.github.com>
2026-04-22 11:00:48 -0500

    add print_profiled()

--- src/gio_close.c
+++ src/gio_close.c
@@ -11,6 +11,47 @@
 
 #include "gioi.h"
 
+#if GIO_PROFILING_MODE == 1
+
+#include <stdio.h>
+
+static
+void print_profiled(MPI_Comm comm)
+{
+    int i, rank;
+    double max_t[NTIMERS];
+    MPI_Count max_c[NTIMERS];
+
+    MPI_Comm_rank(comm, &rank);
+
+    /* collect two-phase I/O timers */
+    MPI_Reduce(gio_wr_time, max_t, NTIMERS, MPI_DOUBLE, MPI_MAX, 0, comm);
+    for (i=0; i<NTIMERS; i++) gio_wr_time[i] = max_t[i];
+
+    MPI_Reduce(gio_rd_time, max_t, NTIMERS, MPI_DOUBLE, MPI_MAX, 0, comm);
+    for (i=0; i<NTIMERS; i++) gio_rd_time[i] = max_t[i];
+
+    MPI_Reduce(gio_wr_count, max_c, NTIMERS, MPI_COUNT, MPI_MAX, 0, comm);
+    for (i=0; i<NTIMERS; i++) gio_wr_count[i] = max_c[i];
+
+    MPI_Reduce(gio_rd_count, max_c, NTIMERS, MPI_COUNT, MPI_MAX, 0, comm);
+    for (i=0; i<NTIMERS; i++) gio_rd_count[i] = max_c[i];
+
+    /* print 2-phase write timers */
+    if (rank == 0 && gio_wr_count[0] > 0) {
+        printf("write time: init %.2f pwrite %.2f pread %.2f post %.2f hsort %.2f comm %.2f total %.2f\n",
+        gio_wr_time[1], gio_wr_time[2], gio_rd_time[2], gio_wr_time[4], gio_wr_time[5], gio_wr_time[3], gio_wr_time[0]);
+        printf("write count: ntimes %lld check_hole %lld (npairs %lld nrecv %lld) no check %lld (npairs %lld nrecv %lld)\n",
+        gio_wr_count[0], gio_wr_count[1], gio_wr_count[2], gio_wr_count[3], gio_wr_count[4], gio_wr_count[5], gio_wr_count[6]);
+    }
+
+    /* print 2-phase read timers */
+    if (rank == 0 && gio_rd_count[0] > 0)
+        printf("read count: init %.2f pread %.2f post %.2f wait %.2f total %.2f (ntimes %lld)\n",
+        gio_rd_time[1], gio_rd_time[2], gio_rd_time[3], gio_rd_time[4], gio_rd_time[0], gio_rd_count[0]);
+}
+#endif
+
 /*----< GIO_close() >------------------------------------------------------*/
 int GIO_close(GIO_File *fh)
 {
